5914f6a21c8391f86e1be5ffebce7f186b58d368
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-27 18:38:18 +0200

    CatalogPage

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
 import Location from '../../components/Location/Location.jsx'
-import VehicleEquipment from '../../components/VehicleEquipment/VehicleEquipment.JSX'
 import VehicleType from '../../components/VehicleType/VehicleType.jsx'
 import { fetchCampers } from '../../redux/campers/operations.js'
 import CamperPage from '../CamperPage/CamperPage.jsx'
@@ -8,6 +7,7 @@ import CamperPage from '../CamperPage/CamperPage.jsx'
 import css from './CatalogPage.module.css' 
 import CampersList from '../../components/CamperList/CampersList.jsx'
 import { useEffect } from 'react'
+import VehicleEquipment from '../../components/VehicleEquipment/VehicleEquipment.JSX'
 export default function CatalogPage () {
     const dispatch = useDispatch();
     const campers = useSelector(state => state.campers.items);
@@ -20,7 +20,7 @@ return(
     <div className={css.sidebar}>
     <Location />
      <p className={css.filters}>Filters</p>
-     <VehicleEquipment/>
+     <VehicleEquipment />
      <VehicleType />
     </div>
     <div className={css.camper}>
