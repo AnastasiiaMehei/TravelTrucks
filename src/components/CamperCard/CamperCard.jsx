9fce6710a0262526fb10658ceeb6c3e6547a655c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-27 20:53:18 +0200

    CamperCard

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -1,31 +1,22 @@
+// import { useDispatch } from 'react-redux';
 import css from './CamperCard.module.css'
 export  default function CamperCard ({camper}) {
-    const  {name, price, rating, location, description}  = camper;
+    const  {name, rating,  price, location, description, gallery}  = camper;
+    // const dispatch = useDispatch();
     return(
         <div className={css.wrapper}>
             <div className={css.imageContainer}>
-              {camper.gallery.map((image, index) => (
-                <img
-                  key={index}
-                  src={image.thumb}
-                  alt={`Camper ${camper.name} image ${index + 1}`}
-                  className={css.image}
-                />
-              ))}
-            </div>
+            <img src={gallery[0].thumb} alt="Camper's photo" className={css.img} />
+                        </div>
            <div className={css.content}>
             
 <div className={css.divTitle}>
     <h2 className={css.title}>{name}</h2>
     <p className={css.price}>{price}</p>
-    <button className={css.like} type='button'> <svg width="26" height="24">
-                <use href="./images/symbol-defs.svg#icon-like"></use>
-              </svg></button>
+
 </div>
 <div className={css.reviewsLocation}>
-<button className={css.like} type='button'> <svg width="16" height="16">
-                <use href="../../images/symbol-defs.svg#icon-Rating"></use>
-              </svg></button>
+
     <p className={css.reviews}>{rating}</p>
     <p className={css.location}>{location}</p>
 </div>
