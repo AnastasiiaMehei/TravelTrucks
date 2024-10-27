d03ccfd3c0cf4faa8aa4cb665574fb444a5aa37d
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-27 21:46:51 +0200

    CamperCard

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -1,8 +1,17 @@
 // import { useDispatch } from 'react-redux';
 import css from './CamperCard.module.css'
+import { CiHeart } from "react-icons/ci";
+import { RxStarFilled } from "react-icons/rx";
+import { FaRegMap } from "react-icons/fa";
+import { FaEuroSign } from "react-icons/fa";
+
+
+
 export  default function CamperCard ({camper}) {
-    const  {name, rating,  price, location, description, gallery}  = camper;
-    // const dispatch = useDispatch();
+    const  {name, rating,  price, location, description, gallery, reviews }  = camper;
+    const reviewCount = reviews.length;
+    const averageRating = reviews.reduce((sum, review) => sum + review.reviewer_rating, 0) / reviewCount || 0;
+
     return(
         <div className={css.wrapper}>
             <div className={css.imageContainer}>
@@ -12,18 +21,28 @@ export  default function CamperCard ({camper}) {
             
 <div className={css.divTitle}>
     <h2 className={css.title}>{name}</h2>
-    <p className={css.price}>{price}</p>
-
+    <div className={css.priceHeart}>
+    <p className={css.price}><FaEuroSign />
+    {price}</p>
+    <button className={css.heartButton} type='button'>    <CiHeart className={css.heart} />
+    </button>
+    </div>
 </div>
 <div className={css.reviewsLocation}>
 
-    <p className={css.reviews}>{rating}</p>
-    <p className={css.location}>{location}</p>
-</div>
-<div>
-    <p className={css.shortSummary}>{description}
+    <p className={css.reviews}>
+    <RxStarFilled className={css.star} />
+
+    {/* {rating}({}Reviews) */}
+    {averageRating.toFixed(1)} ({reviewCount} reviews)
+
     </p>
+    <p className={css.location}><FaRegMap />
+    {location}</p>
 </div>
+<div className={css.shortSummaryContainer}>
+          <p className={css.shortSummary}>{description}</p>
+        </div>
 <div className={css.links}>
     <a href=""></a>
 </div>
