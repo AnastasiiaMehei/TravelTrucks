3aba25f34d71e2c02abf7fe70c65adc1385ec537
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-28 19:34:18 +0200

    CardDescription

--- src/components/CardDescription/CardDescription.jsx
+++ src/components/CardDescription/CardDescription.jsx
@@ -1,10 +1,34 @@
-export default function CardDescription () {
-    const {name, }
+import { RxStarFilled } from "react-icons/rx";
+import { FaRegMap } from "react-icons/fa";
+import css from './CardDescription.module.css'
+export default function CardDescription ({camper}) {
+    const {name, gallery, reviews, location, description} = camper;
+    const reviewCount = reviews.length;
+    const averageRating = reviews.reduce((sum, review) => sum + review.reviewer_rating, 0) / reviewCount || 0;
+
     return(
         <div>
-            <h2>
+                <h2 className={css.title}>{name}</h2>
+               <div className={css.images}>
+               <img src={gallery[0].thumb} alt="Camper's photo" className={css.img} />
+                <img src={gallery[1].thumb} alt="Camper's photo" className={css.img} />
+                <img src={gallery[2].thumb} alt="Camper's photo" className={css.img} />
+                <img src={gallery[3].thumb} alt="Camper's photo" className={css.img} />
+               </div>
 
-            </h2>
+                <div className={css.reviewsLocation}>
+
+<p className={css.reviews}>
+<RxStarFilled className={css.star} />
+
+{averageRating.toFixed(1)} ({reviewCount} reviews)
+
+</p>
+<p className={css.location}><FaRegMap />
+{location}</p>
+</div>
+<p className={css.shortSummary}>{description}</p>
+
         </div>
     )
 }
