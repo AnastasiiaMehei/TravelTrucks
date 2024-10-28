2c2e6f8ca681f66ff03abeabef4c8bba7df9c4a2
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-10-28 21:14:23 +0200

    CardDescription

--- src/components/CardDescription/CardDescription.jsx
+++ src/components/CardDescription/CardDescription.jsx
@@ -1,35 +1,48 @@
 import { RxStarFilled } from "react-icons/rx";
 import { FaRegMap } from "react-icons/fa";
-import css from './CardDescription.module.css'
-export default function CardDescription ({camper}) {
-    const {name, gallery, reviews, location, description} = camper;
-    const reviewCount = reviews.length;
-    const averageRating = reviews.reduce((sum, review) => sum + review.reviewer_rating, 0) / reviewCount || 0;
+import { FaEuroSign } from "react-icons/fa";
 
-    return(
-        <div>
-            <p className={css.hello}>hello</p>
-                <h2 className={css.title}>{name}</h2>
-               <div className={css.images}>
-               <img src={gallery[0].thumb} alt="Camper's photo" className={css.img} />
+import css from "./CardDescription.module.css";
+export default function CardDescription({ camper }) {
+  const { name, gallery, reviews, location, price, description } = camper;
+  const reviewCount = reviews.length;
+  const averageRating =
+    reviews.reduce((sum, review) => sum + review.reviewer_rating, 0) /
+      reviewCount || 0;
+
+  return (
+    <div className={css.wrapper}>
+    <div className={css.mainInfo}>
+    <h2 className={css.title}>{name}</h2>
+      <div className={css.reviewsLocation}>
+        <p className={css.reviews}>
+          <RxStarFilled className={css.star} />
+          {averageRating.toFixed(1)} ({reviewCount} reviews)
+        </p>
+        <p className={css.location}>
+          <FaRegMap />
+          {location}
+        </p>
+      </div>
+      <p className={css.price}><FaEuroSign />
+      {price}</p>
+    </div>
+      <div className={css.images}>
+                <div className={css.imageDiv}>
+                <img src={gallery[2].original} alt="Camper's photo" className={css.img} />
+
+                </div>
+                {/* <div className={css.imageDiv}>
                 <img src={gallery[1].thumb} alt="Camper's photo" className={css.img} />
-                <img src={gallery[2].thumb} alt="Camper's photo" className={css.img} />
-                <img src={gallery[3].thumb} alt="Camper's photo" className={css.img} />
-               </div>
 
-                <div className={css.reviewsLocation}>
+                </div>
+<div className={css.imageDiv}>
+<img src={gallery[2].thumb} alt="Camper's photo" className={css.img} />
 
-<p className={css.reviews}>
-<RxStarFilled className={css.star} />
+</div> */}
 
-{averageRating.toFixed(1)} ({reviewCount} reviews)
-
-</p>
-<p className={css.location}><FaRegMap />
-{location}</p>
 </div>
-<p className={css.shortSummary}>{description}</p>
-
-        </div>
-    )
+      <p className={css.shortSummary}>{description}</p>
+    </div>
+  );
 }
--- src/pages/CamperPage/CamperPage.jsx
+++ src/pages/CamperPage/CamperPage.jsx
@@ -10,34 +10,33 @@ import { useParams } from "react-router-dom";
 // import CamperDetails from "../../components/CamperDetails/CamperDetails";
 
 import css from './CamperPage.module.css'
+import { selectCamperById } from '../../redux/campers/selectors';
+
 import CardDescription from "../../components/CardDescription/CardDescription";
 export default function CamperPage(){
   const { camperId } = useParams();
+
+  const camper = useSelector(selectCamperById);
   const dispatch = useDispatch();
-  const camper = useSelector(state => state.campers.currentCamper);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isDataLoading, setIsDataLoading] = useState(true);
 
 
   useEffect(() => {
-    if (camperId) { 
-      dispatch(fetchCamperById(camperId))
-        .unwrap()
-        .then(() => setIsLoading(false))
-        .catch(() => setIsLoading(false));
+    try {
+      dispatch(fetchCamperById(camperId));
+    } catch (e) {
+      console.log(e.message);
+    } finally {
+      setIsDataLoading(false);
     }
-  }, [dispatch, camperId]);;
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!camper) {
-    return <div>Camper not found</div>;
-  }
+  }, [dispatch, camperId]);
 
 
   return (
     <div className={css.container}>
-      <CardDescription camper={camper} />
+      {isDataLoading ? ( <div>Loading...</div>
+      ) : (<CardDescription camper={camper} />)}
+
     </div>
   );
 }
