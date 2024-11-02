69b888211dd392a3dcf6dd462bddce65abc474f2
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-11-02 15:35:51 +0200

    API

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,34 +1,31 @@
-import {  useDispatch, useSelector } from 'react-redux'
-import Location from '../../components/Location/Location.jsx'
-import VehicleType from '../../components/VehicleType/VehicleType.jsx'
-import { selectFilters } from '../../redux/filters/selectors';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import Location from "../../components/Location/Location.jsx";
+import VehicleType from "../../components/VehicleType/VehicleType.jsx";
+import CampersList from "../../components/CamperList/CampersList.jsx";
 
-// import CamperPage from '../CamperPage/CamperPage.jsx'
-import CampersList from '../../components/CamperList/CampersList.jsx'
-import css from './CatalogPage.module.css' 
-import { useEffect } from 'react';
-import { fetchCampers } from '../../redux/campers/operations.js';
-export default function CatalogPage () {
+import { selectFilters } from "../../redux/filters/selectors";
+import { fetchCampers } from "../../redux/campers/operations.js";
+import css from "./CatalogPage.module.css";
+
+export default function CatalogPage() {
   const dispatch = useDispatch();
   const filters = useSelector(selectFilters);
-  const campers = useSelector(state => state.campers.items);
+  const campers = useSelector((state) => state.campers.items);
+
   useEffect(() => {
     dispatch(fetchCampers(filters));
   }, [dispatch, filters]);
-
-return(
+  return (
     <div className={css.wrapper}>
-    <div className={css.sidebar}>
-    <Location />
-     <p className={css.filters}>Filters</p>
-
-     <VehicleType />
+      <div className={css.sidebar}>
+        <Location />
+        <p className={css.filters}>Filters</p>
+        <VehicleType />
+      </div>
+      <div className={css.camper}>
+        <CampersList campers={campers} />
+      </div>
     </div>
-    <div className={css.camper}>
-    <CampersList campers={campers} />
-
-        {/* <CamperPage/> */}
-    </div>
-    </div>
-)
-};
+  );
+}
--- src/redux/campers/operations.js
+++ src/redux/campers/operations.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
+
 export const fetchCampers = createAsyncThunk(
   "campers/fetchAll",
   async (filters, thunkAPI) => {
     try {
-      const { location, form, features } = filters;
+      const { location, form, transmission, features } = filters;
 
       // Створення об'єкта параметрів запиту
       const params = {};
@@ -17,26 +18,34 @@ export const fetchCampers = createAsyncThunk(
       if (form) {
         params.form = form;
       }
-      if (features && features.length > 0) {
-        // Передача параметрів у вигляді рядка, розділеного комами
-        params.features = features.join(',');
+      if (transmission) {
+        params.transmission = transmission;
+      }
+      if (features) {
+        // Додаємо кожну функцію як окремий параметр, якщо вона встановлена в true
+        Object.keys(features).forEach((feature) => {
+          if (features[feature]) {
+            params[feature] = true;
+          }
+        });
       }
 
       const response = await axios.get("/campers", { params });
-      return response.data.items;
+      return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
-  export const fetchCamperById = createAsyncThunk(
-    'campers/fetchById',
-    async (camperId, thunkAPI) => {
-      try {
-        const response = await axios.get(`/campers/${camperId}`);
-        return response.data;
-      } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-      }
+
+export const fetchCamperById = createAsyncThunk(
+  "campers/fetchById",
+  async (camperId, thunkAPI) => {
+    try {
+      const response = await axios.get(`/campers/${camperId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
-  );
+  }
+);
